9b58bd3217102f4127e936d1f4bfd6dfb71211f9
Zhe Kang <zkang@thoughtworks.com>
2020-07-04 08:36:59 +0800

    complete matcher tests

--- src/matchers/__test__/expect.calls.test.js
+++ src/matchers/__test__/expect.calls.test.js
@@ -6,7 +6,8 @@ test("TODO 11", () => {
   const mockCallback = jest.fn((x) => 42 + x);
   forEach([1, 2], mockCallback);
 
-  // TODO 11: add assertion
+  expect(mockCallback).toHaveBeenCalledWith(1);
+  expect(mockCallback).toHaveBeenCalledWith(2);
 });
 
 test("TODO 12", () => {
@@ -16,9 +17,8 @@ test("TODO 12", () => {
     .mockReturnValueOnce("string")
     .mockReturnValue(true);
 
-  // TODO 12: to add "expected" value
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
+  expect(mockFn()).toBe(42);
+  expect(mockFn()).toBe("string");
+  expect(mockFn()).toBe(true);
+  expect(mockFn()).toBe(true);
 });
